59cac85c310c08d1f121ad397833fd1675abfcec
Michael Mathias <michaelscodingjourney@gmail.com>
2023-04-28 13:30:33 -0700

    added click event on ramen image to display ramen content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-// write your code here
+const ramenMenu = document.getElementById('ramen-menu');
+const ramenDetailDiv = document.querySelector('#ramen-detail');
+const ramenDetailImage = document.querySelector('.detail-image');
+const ramenDetailH2 = document.querySelector('.name');
+const ramenDetailRestaurant = document.querySelector('.restaurant');
+const ramenRating = document.querySelector('#rating-display');
+const ramenComment = document.querySelector('#comment-display');
+
+fetch('http://localhost:3000/ramens')
+.then(response => response.json())
+.then(data => {
+
+    data.forEach(ramen => {
+        const image = document.createElement('img');
+        image.setAttribute('class', 'ramen-image');
+        image.src = ramen.image;
+        
+        ramenMenu.appendChild(image);
+        
+        image.addEventListener('click', e => {
+            const addImage = e.target.src;
+
+            ramenDetailH2.textContent = ramen.name;
+            ramenDetailRestaurant.textContent = ramen.restaurant;
+            ramenRating.textContent = ramen.rating;
+            ramenComment.textContent = ramen.comment;
+
+            ramenDetailImage.src = addImage;
+        })
+    });
+})
+.catch(error => {
+    console.log(error);
+})
